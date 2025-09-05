10fd25ef73fffc917b4cb9289e13ce675d4f8ae4
Tanner Watmough <79290989+tannerwatmough@users.noreply.github.com>
2025-09-04 18:39:49 -0600

    add player rotation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 from constants import *
-from player import *
+from player import Player
 
 def main():
     pygame.init()
@@ -9,26 +9,25 @@ def main():
     dt = 0
     
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
-    x = SCREEN_WIDTH / 2
-    y = SCREEN_HEIGHT / 2
-    player = Player(x, y)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     while True:
+        # enable exiting window
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        # Screen redraw
         screen.fill("black")
+        player.update(dt)
         player.draw(screen)
-
         pygame.display.flip()
-        dt = clock.tick(60)
-        dt /= 1000
 
+        # limit framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 if __name__ == "__main__":
     main()
