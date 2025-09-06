8b326689ea2af52318ee171d2e3941aef88b5689
Tanner Watmough <79290989+tannerwatmough@users.noreply.github.com>
2025-09-06 14:42:06 -0600

    added spawning and moving asteroids

---
+++ asteroid.py
@@ -0,0 +1,13 @@
+import pygame
+from circleshape import CircleShape
+
+class Asteroid(CircleShape):
+  def __init__(self, x, y, radius):
+    super().__init__(x, y, radius)
+
+  def draw(self, screen):
+    width = 2
+    pygame.draw.circle(screen, "white", self.position, self.radius, width)
+
+  def update(self, dt):
+    self.position += self.velocity * dt 
