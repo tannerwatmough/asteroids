e6b1d3677799a6dca44e6b0d7d0b45edd370ae6a
Tanner Watmough <79290989+tannerwatmough@users.noreply.github.com>
2025-09-07 00:02:51 -0600

    added scoring, lives, respawning, and game over screen

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, time
 from constants import *
 from player import Player
 from asteroid import Asteroid
@@ -12,8 +12,11 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     counter = Counter()
+    timer = 0
     
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    background = pygame.image.load("./background.png")
+    font = pygame.font.Font(None, 28)
     
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
@@ -40,12 +43,37 @@ def main():
 
         # Screen redraw
         screen.fill("black")
+        screen.blit(background, (0, 0))
+
+        lives_text = pygame.font.Font.render(font, f"Lives Left: {player.lives}", 1, "white")
+        score_text = pygame.font.Font.render(font, f"Score: {counter.score}", 1, "white")
+        screen.blit(lives_text, (SCREEN_WIDTH / 2, 10))
+        screen.blit(score_text, (SCREEN_WIDTH / 2 + 200, 10))
+
         updatable.update(dt)
         for asteroid in asteroids:
-            if asteroid.collision(player):
-                print("Game over!")
-                print(f"Final Score: {counter.score}")
-                return
+            if asteroid.collision(player) and timer <= 0:
+                player.lives -= 1
+                player.position = pygame.Vector2(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+                print(f"{player.lives} lives left!")
+                timer = 100
+                if player.lives <= 0:
+                    print("Game over!")
+                    print(f"Final Score: {counter.score}")
+                    
+                    game_over_text = pygame.font.Font.render(font, "Game over!", 1, "white")
+                    score_text = pygame.font.Font.render(font, f"Final Score: {counter.score}", 1, "white")
+
+                    screen.blit(game_over_text, (SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2))
+                    screen.blit(score_text, (SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 + 35))
+        
+                    for obj in drawable:
+                        obj.draw(screen)
+                    
+                    pygame.display.flip()
+                    time.sleep(10)
+
+                    return
             for shot in shots:
                 if asteroid.collision(shot):
                     shot.kill()
@@ -56,5 +84,7 @@ def main():
 
         # limit framerate to 60 FPS
         dt = clock.tick(60) / 1000
+        timer -= 1
+
 if __name__ == "__main__":
     main()
--- player.py
+++ player.py
@@ -8,6 +8,7 @@ class Player(CircleShape):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
         self.timer = 0
+        self.lives = 3
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
